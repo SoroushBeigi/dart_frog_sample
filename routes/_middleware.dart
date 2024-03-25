423fe1c7c7fcda521a1d92e35de5b806f566dfe4
s.masoumbeigi <sbeygi712@gmail.com>
2024-03-25 10:11:29 +0330

    main middleware config

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,6 +1,11 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:tasklist_backend/items/items_repository.dart';
+import 'package:tasklist_backend/lists/list_repository.dart';
 
 Handler middleware(Handler handler) {
   // TODO: implement middleware
-  return handler.use(requestLogger()).use(provider<String>((context) => 'Hello World!'));
+  return handler
+      .use(requestLogger())
+      .use(provider<TaskListRepository>((context) => TaskListRepository()))
+      .use(provider<TaskItemRepository>((context) => TaskItemRepository()));
 }
