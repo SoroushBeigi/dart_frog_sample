210c849e0bda639b7b540e26f2178e85339d368d
s.masoumbeigi <sbeygi712@gmail.com>
2024-03-25 10:32:29 +0330

    handling firebase requests

--- routes/db/fbase/_middleware.dart
+++ routes/db/fbase/_middleware.dart
@@ -1,6 +1,13 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:firedart/firedart.dart';
 
 Handler middleware(Handler handler) {
   // TODO: implement middleware
-  return handler;
+  return (context)async {
+    if(!Firestore.initialized) {
+      Firestore.initialize('tasklist-dartfrog-cc1e0');
+    }
+    final response = await handler(context);
+    return response;
+  };
 }
