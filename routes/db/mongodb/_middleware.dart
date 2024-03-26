69dab1aaeed3efbfdcb22021cebcea6858ad8d33
s.masoumbeigi <sbeygi712@gmail.com>
2024-03-26 09:53:27 +0330

    Implementing mongoDB get and post requests

--- routes/db/mongodb/_middleware.dart
+++ routes/db/mongodb/_middleware.dart
@@ -5,5 +5,11 @@ import 'package:tasklist_backend/secrets/secrets.dart';
 Handler middleware(Handler handler) {
   return (context) async{
     final db = await Db.create(Secrets.mongodbSecret);
+    if(!db.isConnected){
+      await db.open();
+    }
+    final response = await handler.use(provider<Db>((_) => db)).call(context);
+    await db.close();
+    return response;
   };
 }
