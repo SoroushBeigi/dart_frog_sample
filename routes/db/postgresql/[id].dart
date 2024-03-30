260b314d48f697bfd911536dc07ba57c8e136959
s.masoumbeigi <sbeygi712@gmail.com>
2024-03-30 16:06:40 +0330

    Implementing postgresql delete request

--- routes/db/postgresql/[id].dart
+++ routes/db/postgresql/[id].dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:dart_frog/dart_frog.dart';
 import 'package:postgres/postgres.dart';
@@ -9,7 +10,7 @@ Future<Response> onRequest(
 ) async {
   return switch (context.request.method) {
     HttpMethod.patch => _updateList(context, id),
-    // HttpMethod.delete => _deleteList(context, id),
+    HttpMethod.delete => _deleteList(context, id),
     _ => Future.value(Response(statusCode: HttpStatus.methodNotAllowed)),
   };
 }
@@ -18,20 +19,33 @@ Future<Response> _updateList(RequestContext context, String id) async {
   final body = await context.request.json() as Map<String, dynamic>;
   final name = body['name'] as String?;
   if (name != null) {
-    try{
+    try {
       final result = await context
-        .read<Connection>()
-        .execute("UPDATE list SET name = '$name' WHERE id = '$id'");
-        if(result.affectedRows == 1){
-          return Response.json(body: 'Successfully updated to $name');
-        }else{
-          return Response.json(body: 'Updating failed');
-        }
-    }catch(e){
+          .read<Connection>()
+          .execute("UPDATE list SET name = '$name' WHERE id = '$id'");
+      if (result.affectedRows == 1) {
+        return Response.json(body: 'Successfully updated to $name');
+      } else {
+        return Response.json(body: 'Updating failed');
+      }
+    } catch (e) {
       return Response(statusCode: HttpStatus.connectionClosedWithoutResponse);
     }
   }
   return Response(statusCode: HttpStatus.badRequest);
 }
 
-// Future<Response> _deleteList(RequestContext context, String id) async {}
+Future<Response> _deleteList(RequestContext context, String id) async {
+  await context
+      .read<Connection>()
+      .execute("DELETE FROM list WHERE id =  '$id '")
+      .then(
+    (value) {
+      return Response(statusCode: HttpStatus.noContent);
+    },
+    onError: (e) {
+      return Response(statusCode: HttpStatus.badRequest);
+    },
+  );
+  return Response(statusCode: HttpStatus.badRequest);
+}
