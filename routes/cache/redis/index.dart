ab0f96db29b17767e0d73e81869dcdbe6201c749
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-02 14:07:57 +0330

    Implementing redis get request

--- routes/cache/redis/index.dart
+++ routes/cache/redis/index.dart
@@ -1,6 +1,27 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:io';
 
-Response onRequest(RequestContext context) {
-  final greeting = context.read<String>();
-  return Response(body: greeting);
+import 'package:dart_frog/dart_frog.dart';
+import 'package:redis/redis.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    HttpMethod.get => _getLoginStatus(context),
+    // HttpMethod.post => _setLoginStatus(context),
+    _ => Future.value(Response(statusCode: HttpStatus.methodNotAllowed)),
+  };
 }
+
+Future<Response> _getLoginStatus(RequestContext context) async {
+  final result = await context
+      .read<Command>()
+      .send_object(['GET', 'loggedIn']).then((value) => value);
+      if(result==null){
+        const status = 0;
+        await context.read<Command>().send_object(['SET','loggedIn',status]);
+        return Response(statusCode: HttpStatus.noContent);
+      }else{
+        return Response.json(body: result.toString());
+      }
+}
+
+// Future<Response> _setLoginStatus(RequestContext context) async {}
