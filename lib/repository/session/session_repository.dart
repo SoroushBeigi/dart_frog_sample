8afcc9ea8f7496ff09e312f23ce162274c2dd107
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-07 16:51:01 +0330

    creating session repository

--- lib/repository/session/session_repository.dart
+++ lib/repository/session/session_repository.dart
@@ -1,5 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:tasklist_backend/hash_extension.dart';
 
 @visibleForTesting
 Map<String, Session> sessionDatabase = {};
@@ -7,16 +8,34 @@ Map<String, Session> sessionDatabase = {};
 class Session extends Equatable {
   const Session({
     required this.token,
-    required this.userID,
+    required this.userId,
     required this.createdAt,
     required this.expireDate,
   });
 
   final String token;
-  final String userID;
+  final String userId;
   final DateTime createdAt;
   final DateTime expireDate;
 
   @override
-  List<Object?> get props => [token, userID, createdAt, expireDate];
+  List<Object?> get props => [token, userId, createdAt, expireDate];
+}
+
+class SessionRepository {
+  Session createSession(String userId) {
+    final now = DateTime.now();
+    final session = Session(
+      token: generateToken(userId),
+      userId: userId,
+      createdAt: now,
+      expireDate: now.add(const Duration(days: 1)),
+    );
+    sessionDatabase[session.token] = session;
+    return session;
+  }
+
+  String generateToken(String userId) {
+    return '${userId}_${DateTime.now().toIso8601String()}'.hashValue;
+  }
 }
