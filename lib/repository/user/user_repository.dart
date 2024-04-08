44567797f636b6b639616776d879588f59687387
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-08 15:39:37 +0330

    creating user model and user repository

---
+++ lib/repository/user/user_repository.dart
@@ -0,0 +1,7 @@
+class User{
+  const User({required this.id,required this.name,required this.username,required this.password,});
+  final String id;
+  final String name;
+  final String username;
+  final String password;
+}
