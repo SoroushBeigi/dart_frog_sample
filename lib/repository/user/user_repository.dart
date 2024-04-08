fee6d304a465ebcf1a107c91751c8bc91e097997
s.masoumbeigi <sbeygi712@gmail.com>
2024-04-08 15:41:06 +0330

    creating basic auth route and code refactor

--- lib/repository/user/user_repository.dart
+++ lib/repository/user/user_repository.dart
@@ -1,7 +1,74 @@
-class User{
-  const User({required this.id,required this.name,required this.username,required this.password,});
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:tasklist_backend/hash_extension.dart';
+
+@visibleForTesting
+Map<String, User> userDatabase = {};
+
+class User extends Equatable {
+  const User({
+    required this.id,
+    required this.name,
+    required this.userName,
+    required this.password,
+  });
   final String id;
   final String name;
-  final String username;
+  final String userName;
   final String password;
+
+  @override
+  List<Object?> get props => [id, name, userName, password];
+}
+
+class UserRepository {
+  User? userFromCredentials(String username, String password) {
+    final hashedPassword = password.hashValue;
+    final users = userDatabase.values.where((element) =>
+        element.userName == username && element.password == hashedPassword);
+    if (users.isNotEmpty) {
+      return users.first;
+    } else {
+      return null;
+    }
+  }
+
+  User? userFromId(String id) => userDatabase[id];
+
+  String createUser({
+    required String name,
+    required String userName,
+    required String password,
+  }) {
+    final id = userName.hashValue;
+    final user = User(
+      id: id,
+      name: name,
+      userName: userName,
+      password: password.hashValue,
+    );
+    userDatabase[id] = user;
+    return id;
+  }
+
+  void deleteUser(String id) => userDatabase.remove(id);
+
+  Future<void> updateUser({
+    required String id,
+    required String? name,
+    required String? userName,
+    required String? password,
+  }) async {
+    final currentUser = userDatabase[id];
+    if (currentUser == null) {
+      return Future.error(Exception('User not found!'));
+    }
+    final user = User(
+      id: id,
+      name: name ?? currentUser.name,
+      userName: userName ?? currentUser.userName,
+      password: password ?? currentUser.password,
+    );
+    userDatabase[id] = user;
+  }
 }
